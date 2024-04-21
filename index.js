681b6b1aab91c95ed056390e440a9348a6290d60
NgocMinhThuNguyen <94945964+NgocMinhThuNguyen@users.noreply.github.com>
2024-04-21 23:18:57 +0800

    update order

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const disclosureBtns = document.querySelectorAll(".disclosure-btn");
 const options = document.querySelectorAll("[type=radio]");
 const grindOptText = document.querySelector("[data-name=grind-opt]");
 
+const orderSummary = {
+  "preferences": "",
+  "bean-types": "",
+  "quantity": "",
+  "grind-opt": "",
+  "delivery": "",
+  "shipping-price": "",
+}
+
 function toggleDisplay(targetElement, isVisible) {
   if (isVisible.hasAttribute("data-visible")) {
     targetElement.setAttribute("aria-expanded", false);
@@ -14,11 +23,70 @@ function toggleDisplay(targetElement, isVisible) {
   isVisible.toggleAttribute("data-visible");
 }
 
-function displayPlan(option) {
-  const optionName = option.getAttribute("name");
-  const blank = document.querySelector(`[data-name=${optionName}]`);
+function checkOrder(orderSummary) {
+  const grindDisclosureBtn = document.querySelector("[aria-control=grind-opt");
+  const grindContent = document.querySelector("#grind-opt");
+  
+  if (orderSummary["preferences"] === "capsule") {
+    grindDisclosureBtn.setAttribute("disabled", null);
+    grindDisclosureBtn.setAttribute("aria-expaned", false);
+    grindContent.removeAttribute("data-visible");
+    orderSummary["grind-opt"] = "";
+    grindInputs = grindContent.querySelectorAll("input");
+    grindInputs.forEach(grindInput => {
+      grindInput.checked = false;
+    })
+  } else {
+    grindDisclosureBtn.removeAttribute("disabled", null);
+  }
 
-  blank.innerHTML = `${option.dataset.value}`;
+  if(orderSummary["quantity"] !== "") {
+    updateShippingPrice(orderSummary["quantity"])
+  }
+}
+
+function updateShippingPrice(number) {
+  const shippingPrices = {
+    "250": {
+      "every-week": "7.20",
+      "every-2-week": "9.60",
+      "every-month": "12.00",
+    },
+    "500": {
+      "every-week": "13.00",
+      "every-2-week": "17.50",
+      "every-month": "22.00",
+    },
+    "1000": {
+      "every-week": "22.00",
+      "every-2-week": "32.00",
+      "every-month": "42.00",
+    }
+  }
+
+  const frequencies = document.querySelectorAll("[name=delivery] + label");
+  frequencies.forEach( frequency => {
+    const shipPrice = frequency.querySelector(".shipment-price");
+    shipPrice.textContent = `${shippingPrices[number][frequency.getAttribute("for")]}`;
+  });
+  orderSummary["shipping-price"] = shippingPrices[number][orderSummary["delivery"]];
+}
+
+function displayOrder(optionName, orderSummary) {
+  const blank = document.querySelector(`[data-name=${optionName}]`);
+  const grindOptBlank = document.querySelector("[data-name=grind-opt");
+  
+  if (orderSummary["preferences"] === "capsule") {
+    grindOptBlank.classList.add("hidden");
+  } else {
+    grindOptBlank.classList.remove("hidden");
+  }
+
+  if (optionName === "quantity") {
+    blank.innerHTML = `${orderSummary[optionName]}g`;
+  } else {
+    blank.innerHTML = `${orderSummary[optionName].replace(/-/g, " ")}`;
+  } 
 }
 
 // Mobile menu disclosure
@@ -38,56 +106,9 @@ disclosureBtns.forEach( (disclosureBtn) => {
 // Selection update
 options.forEach( (option) => {
   option.addEventListener("change", () => {
-    checkOption(option);
-    displayPlan(option);
+    optionName = option.getAttribute("name");
+    orderSummary[optionName] = option.dataset.value;
+    checkOrder(orderSummary);
+    displayOrder(optionName, orderSummary);
   })
 })
-
-function checkOption(option) {
-  const grindOptDisclosure = document.querySelector("[aria-control=grind-opt");
-  const grindOptContent = document.querySelector(`#${grindOptDisclosure.getAttribute("aria-control")}`);
-  const optionValue = option.dataset.value;
-
-
-  // "Capsule" selection
-  if ( optionValue === "Capsule") {
-    grindOptDisclosure.setAttribute("disabled", null);
-    grindOptDisclosure.setAttribute("aria-expaned", false);
-    grindOptContent.removeAttribute("data-visible");
-    grindOptText.classList.add("hidden");
-  } else {
-    grindOptDisclosure.removeAttribute("disabled", null);
-    grindOptText.classList.remove("hidden");
-  }
-
-  // Weight selection
-  if (option.getAttribute("name") === "quantity") {
-    updateShipment(optionValue);
-  }
-}
-
-function updateShipment(optionValue) {
-  const shippingPrices = {
-    "250g": {
-      "every-week": "7.20",
-      "every-2-week": "9.60",
-      "every-month": "12.00",
-    },
-    "500g": {
-      "every-week": "13.00",
-      "every-2-week": "17.50",
-      "every-month": "22.00",
-    },
-    "1000g": {
-      "every-week": "22.00",
-      "every-2-week": "32.00",
-      "every-month": "42.00",
-    }
-  }
-
-  const frequencies = document.querySelectorAll("[name=delivery] + label");
-  frequencies.forEach( frequency => {
-    const shipPrice = frequency.querySelector(".shipment-price");
-    shipPrice.textContent = `${shippingPrices[optionValue][frequency.getAttribute("for")]}`;
-  });
-}
