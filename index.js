6a4348c713f825aea084a0b67dd0d284167e3017
NgocMinhThuNguyen <94945964+NgocMinhThuNguyen@users.noreply.github.com>
2024-04-23 01:50:41 +0800

    calculate total cost and add modal closing js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const grindOptText = document.querySelector("[data-name=grind-opt]");
 const planForm = document.querySelector(".plan-form");
 const modal = document.querySelector("#modal");
 const submitBtn = document.querySelector("[type=submit]");
+const closeModalBtns = document.querySelectorAll(".close-modal-btn");
 
 const orderSummary = {
   "preferences": "",
@@ -70,48 +71,59 @@ function updateShippingPrice(number) {
 
   const frequencies = document.querySelectorAll("[name=delivery] + label");
 
+  // update shipping price
   frequencies.forEach( frequency => {
     const shipPrice = frequency.querySelector(".shipment-price");
     shipPrice.textContent = `${shippingPrices[number][frequency.getAttribute("for")]}`;
   });
 
+  // calculate shipping cost
   orderSummary["shipping-price"] = shippingPrices[number][orderSummary["delivery"]];
-  if (orderSummary["delivery"] === "every-week") {
-    orderSummary["total-cost"] = (parseFloat(orderSummary["shipping-price"])*4);;
-  } else if (orderSummary["delivery"] === "every-2-week") {
-    orderSummary["total-cost"] = (parseFloat(orderSummary["shipping-price"])*2);
-  } else if (orderSummary["delivery"] === "every-month") {
-    orderSummary["total-cost"] = parseFloat(orderSummary["shipping-price"]);
-  }
+  let shippingPrice = parseFloat(orderSummary["shipping-price"]*100);
+
+    if (orderSummary["delivery"] === "every-week") {
+      orderSummary["total-cost"] = ((shippingPrice*4)/100).toFixed(2).toString();
+    } else if (orderSummary["delivery"] === "every-2-week") {
+      orderSummary["total-cost"] = ((shippingPrice*2)/100).toFixed(2).toString();
+    } else if (orderSummary["delivery"] === "every-month") {
+      orderSummary["total-cost"] = (shippingPrice/100).toFixed(2).toString();
+    }
 }
 
 function displayOrder(optionName, orderSummary) {
   const blanks = document.querySelectorAll(`[data-name=${optionName}]`);
   const grindOptBlanks = document.querySelectorAll("[data-name=grind-opt");
-  
-  if (orderSummary["preferences"] === "capsule") {
-    grindOptBlanks.forEach ( grindOptBlank => {
-      grindOptBlank.classList.add("hidden");
-      grindOptBlank.textContent = "";
+
+  if (optionName === "preferences") {
+    if (orderSummary[optionName] === "capsule") {
+      grindOptBlanks.forEach ( grindOptBlank => {
+        grindOptBlank.classList.add("hidden");
+        grindOptBlank.textContent = "";
+      }) 
+    } else {
+      grindOptBlanks.forEach ( grindOptBlank => {
+        grindOptBlank.classList.remove("hidden");
+        grindOptBlank.textContent = "_____";
+      })
+    }
+    blanks.forEach(blank => {
+      blank.textContent = `${orderSummary[optionName].replace(/-/g, " ")}`;
+    });
+  } else if (optionName === "quantity") { 
+    blanks.forEach(blank => {
+      blank.textContent = `${orderSummary[optionName]}g`; // add "g" unit
     })
-    
   } else {
-    grindOptBlanks.forEach ( grindOptBlank => {
-      grindOptBlank.classList.remove("hidden");
-      grindOptBlank.textContent = "_____";
+    blanks.forEach(blank => {
+      blank.textContent = `${orderSummary[optionName].replace(/-/g, " ")}`; // replace "-" dash with space
     })
   }
 
-  if (optionName === "quantity") {
-    blanks.forEach(blank => {
-      blank.textContent = `${orderSummary[optionName]}g`;
-    })
-    
-  } else {
-    blanks.forEach(blank => {
-      blank.textContent = `${orderSummary[optionName].replace(/-/g, " ")}`;
-    })
-  } 
+  // update total cost
+  totalCosts = document.querySelectorAll("[data-name=shipping-cost");
+  totalCosts.forEach( totalCost => {
+    totalCost.textContent = orderSummary["total-cost"];
+  })
 }
 
 function enableSubmitBtn(submitBtn) {
@@ -159,4 +171,10 @@ options.forEach( (option) => {
 planForm.addEventListener("submit", (e) => {
   e.preventDefault();
   modal.showModal();
+})
+
+closeModalBtns.forEach( closeModalBtn => {
+  closeModalBtn.addEventListener("click", () => {
+    modal.close();
+  })
 })
